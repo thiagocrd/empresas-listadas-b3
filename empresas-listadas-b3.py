8b92526cdc09a39531a0e84dcb57263b5a921e0c
thiagocrd <thiagocrd@users.noreply.github.com>
2020-12-30 20:23:03 +0000

    Update empresas-listadas-b3.py
    Incluí filtro de ações por UNIT
    Comentei o código

--- empresas-listadas-b3.py
+++ empresas-listadas-b3.py
@@ -1,16 +1,22 @@
 import pandas as pd
 
+# Usamos apenas as colunas em que precisamos aplicar filtros
+# O arquivo .csv não vem formatado em utf-8, então usamos o encoding = 'latin'
+# Definimos o dtype da coluna CorpGovnLvlNm como string para não termos problema com a memória
 df = pd.read_csv(
     'InstrumentsConsolidatedFile_20201229_1.csv',
     usecols= ['RptDt','TckrSymb', 'SgmtNm', 'SctyCtgyNm', 'TradgStartDt', 'CorpGovnLvlNm'],
     sep=';',
-    encoding = "latin",
+    encoding = 'latin',
     dtype={'CorpGovnLvlNm': 'str'})
-df = df[ (df['SctyCtgyNm'] == 'SHARES') & (df['SgmtNm'] == 'CASH') & (df['RptDt'] > df['TradgStartDt']) & (df['CorpGovnLvlNm'] != 'MERCADO DE BALCÃO') ]
 
-df2 = pd.DataFrame()
-df2['Ticker'] = df['TckrSymb']
-df2['Ticker_Prefix'] = df['TckrSymb'].str[:4]
-df2.drop_duplicates('Ticker_Prefix', inplace=True)
+# Filtramos SctyCtgyNm por ações e units (SHARES e UNIT)
+# Filtramos SgmtNm por ações do mercado à vista (CASH)
+# Filtramos os ativos cuja data de início da negociação seja anterior à data de publicação do arquivo .csv
+# Filtramos as ações que não sejam negociadas no mercado de balcão
+df = df[ ( (df['SctyCtgyNm'] == 'SHARES') | (df['SctyCtgyNm'] == 'UNIT') ) & (df['SgmtNm'] == 'CASH') & (df['RptDt'] > df['TradgStartDt']) & (df['CorpGovnLvlNm'] != 'MERCADO DE BALCÃO') ]
 
-print(df2['Ticker'])
+df_listed_stocks = pd.DataFrame()
+df_listed_stocks['Ticker'] = df['TckrSymb']
+
+print(df_listed_stocks['Ticker'])
